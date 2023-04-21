2780a58599297f4bdb8bcef017f7cc60ba2fb4a9
Emery Berger <emery.berger@gmail.com>
2023-04-21 18:40:29 -0400

    More descriptive name for database file.

--- snakelang/snakelang_module.py
+++ snakelang/snakelang_module.py
@@ -65,7 +65,7 @@ def complete(user_prompt):
 def spec(string):
     def decorator(func):
         cached_function = None
-        cdb = CodeDatabase("snake.db")
+        cdb = CodeDatabase("snakelang-cache.db")
         def wrapper(*args, **kwargs):
             nonlocal cdb, cached_function
             if cached_function:
