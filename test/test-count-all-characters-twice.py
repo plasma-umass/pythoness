4a946cfcaca46b484562c448f20a56d86f19d668
Emery Berger <emery.berger@gmail.com>
2023-04-22 16:34:07 +0200

    New test.

---
+++ test/test-count-all-characters-twice.py
@@ -0,0 +1,18 @@
+import pythoness
+
+@pythoness.spec("Given a string s and a character c, return the number of times c appears as a consecutive pair in s.",
+                tests = ["count_chars('hello', 'e') == 0",
+                         "count_chars('hello', 'l') == 1",
+                         "count_chars('helll', 'l') == 2",
+                         "count_chars('what is up', 'q') == 0"],
+                max_retries=3)
+def count_chars(s: str, c) -> int:
+    ""
+
+print("Running tests.")
+assert(count_chars("hello, this is a test.", "t") == 0)
+assert(count_chars("hello, this is a test.", "e") == 0)
+assert(count_chars("hello, this is a test.", "s") == 0)
+assert(count_chars("hello, this is a test.", "l") == 1)
+assert(count_chars("hello, this is a test.", "q") == 0)
+print("Tests complete.")
