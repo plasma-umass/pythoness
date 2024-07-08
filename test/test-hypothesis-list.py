b6866930f4187ed18ac28ed3a57e0829c14c919e
trungles <157248098+trungles@users.noreply.github.com>
2024-07-08 16:17:57 -0400

    Delete test directory

--- test/test-hypothesis-list.py
+++
@@ -1,7 +0,0 @@
-import pythoness
-
-@pythoness.spec("Concatenates three lists, l1, l2, l3", tests=[({'l1' : 'lists(text())', 'l2' : 'lists(text())', 'l3' : 'lists(text())'}, "len(l1) + len(l2) + len(l3) == len(triconcat(l1, l2, l3))")])
-def triconcat(l1 : list, l2 : list, l3: list) -> list:
-    ""
-
-print(triconcat(['a'], ['b'], ['c','d']))
--- test/test-infinite-loop.py
+++
@@ -1,8 +0,0 @@
-import pythoness
-
-@pythoness.spec("A function that never terminates", timeout_seconds=10)
-
-def infinite():
-    ""
-
-infinite()
