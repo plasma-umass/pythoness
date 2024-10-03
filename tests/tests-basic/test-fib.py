8b081380ddc9a79c294e68f56a97369dba0b9d8d
Kyla Levin <62115756+ravenblood000@users.noreply.github.com>
2024-10-03 12:22:57 +0000

    black formatting

--- tests/tests-basic/test-fib.py
+++ tests/tests-basic/test-fib.py
@@ -4,6 +4,7 @@ import pythoness
 @pythoness.spec(
     "Compute the nth number in the Fibonacci series.",
     tests=[({"n": "integers(1,20)"}, "myfib(n+2) == myfib(n+1)+myfib(n)")],
+    verbose=True,
 )
 def myfib(n: int) -> int:
     """"""
